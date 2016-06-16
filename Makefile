a96894ea1a0a1346b874caccbcaa38dea34581b6
Brian Palmer <brianp@instructure.com>
2016-06-16 16:01:14 -0600

    initial assignment test

---
+++ Makefile
@@ -0,0 +1,17 @@
+export TEMPDIR := $(shell mktemp -u)
+
+npm_install: package.json
+	npm install
+
+lambda.zip: index.js npm_install
+	echo $(TEMPDIR)
+	mkdir -p $(TEMPDIR)
+	cp index.js $(TEMPDIR)
+	cp -r lib $(TEMPDIR)
+	cp -r node_modules $(TEMPDIR)
+	cd $(TEMPDIR) && zip --quiet -r lambda *
+	cp $(TEMPDIR)/lambda.zip .
+	rm -fr $(TEMPDIR)
+
+upload: lambda.zip
+	aws --region us-east-1 lambda update-function-code --function-name AlexaReadAssignments --zip-file fileb://lambda.zip
---
+++ index.js
@@ -0,0 +1,185 @@
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+ * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
+ * testing instructions are located at http://amzn.to/1LzFrj6
+ *
+ * For additional samples, visit the Alexa Skills Kit Getting Started guide at
+ * http://amzn.to/1LGWsLG
+ */
+
+var AUTH_TOKEN = '<insert token here>';
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
+        if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.[unique-value-here]") {
+             context.fail("Invalid Application ID");
+        }
+        */
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId +
+        ", sessionId=" + session.sessionId);
+}
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId +
+        ", sessionId=" + session.sessionId);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId +
+        ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    } if ("WhatIsMyNextAssignmentIntent" === intentName) {
+        getAssignmentFromCanvas(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+        getWelcomeResponse(callback);
+    } else if ("AMAZON.StopIntent" === intentName || "AMAZON.CancelIntent" === intentName) {
+        handleSessionEndRequest(callback);
+    } else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId +
+        ", sessionId=" + session.sessionId);
+    // Add cleanup logic here
+}
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    var sessionAttributes = {};
+    var cardTitle = "Welcome";
+    var speechOutput = "Welcome to the Alexa Skills Kit sample. " +
+        "Please tell me your favorite color by saying, my favorite color is red";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "Please tell me your favorite color by saying, " +
+        "my favorite color is red";
+    var shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    var cardTitle = "Session Ended";
+    var speechOutput = "Thank you for trying the Alexa Skills Kit sample. Have a nice day!";
+    // Setting this to true ends the session and exits the skill.
+    var shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+function getAssignmentFromCanvas(intent, session, callback) {
+    var request = {
+       uri: 'https://canvas.instructure.com/api/v1/users/self/upcoming_events',
+       headers: {'Authorization': 'Bearer ' + AUTH_TOKEN}
+    };
+    require('request')(request, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        console.log(error);
+        return callback({}, buildSpeechletResponse(intent.name, "I was unable to communicate with Canvas.", null, false));
+      }
+      var assignments = JSON.parse(body);
+      if (assignments.length < 1) {
+        return callback({}, buildSpeechletResponse(intent.name, "You have no upcoming assignments. Spring break!", null, false));
+      }
+      var assignment = assignments[0];
+      var speechOutput = "Your next due assignment is " + assignment.title;
+      var dueAt = (assignment['assignment'] || {})['due_at'];
+      if (dueAt) {
+        var timestamp = require('moment')(dueAt);
+        speechOutput += ", due " + timestamp.fromNow();
+      }
+      return callback({}, buildSpeechletResponse(intent.name, speechOutput, null, false));
+    });
+}
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: "SessionSpeechlet - " + title,
+            content: "SessionSpeechlet - " + output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
